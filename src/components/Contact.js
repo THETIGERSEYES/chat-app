8e33a21ee260aa7f2104e232f632e8359683af4f
THETIGERSEYES <samias120@yahoo.fr>
2020-10-14 02:02:47 +0200

    added contactList to chat-app

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import './Contact.css';
+import PropTypes from 'prop-types';
 
-
-
-
-function Contact() {
-    return(
-        <figure className="Contact">
-            <img  className="avatar" src= "https://randomuser.me/api/portraits/women/35.jpg" alt="avatar-profile" />
-       
-            <div className ="StatusName">
-                <p className="Name">Dianne Hale</p>
-                        <div className ="status">
-                <span className ="status-online"></span>
-                <p className ="status-text"> Online</p>
-            </div>
-            </div>
-        </figure>
-    );  
+function Contact(props) {
+  return (
+    <figure className="Contact">
+      <img src={props.avatar} alt={props.name} className="avatar" />
+      <div className="namedisplay">
+        <p className="name">{props.name}</p>
+        <div className="status">
+          <span
+            className={props.online ? "status-online" : "status-offline"}
+          ></span>
+          <p className="status-text">{props.online ? "Online" : "Offline"}</p>
+        </div>
+      </div>
+    </figure>
+  );
 }
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  online: PropTypes.bool.isRequired
+};
 
 export default Contact;
